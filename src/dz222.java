52f937baf91420f1d7568bbadc437db84a5f6737
payertop <skillsod55@gmail.com>
2022-02-11 00:41:37 +0700

    commit1

---
+++ src/dz222.java
@@ -0,0 +1,33 @@
+public class dz222 {
+    public static void main(String[] args) {
+
+        int balance = 100;
+        int deposit = 1100;
+        int percent = deposit / 100 * 1;
+        {
+            if (deposit >= 1000) {
+
+            }else {
+                if (deposit < 1000)
+                    percent = 0;
+            }
+
+                int bonus = deposit + percent;
+
+                System.out.println(bonus);
+
+
+                // Объявляете переменные для входных данных и
+                // параметров программы: начального счёта,
+                // суммы пополнения и тп
+
+                // Условным оператором проверяете превысила ли
+                // сумма пополнения порог и для этих двух разных
+                // сценариев рассчитываете сумму бонуса и выводите
+                // на экран.
+            }
+
+
+        }
+
+    }
